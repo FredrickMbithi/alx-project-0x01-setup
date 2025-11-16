93dbb862faa8d24e0f1dc475f8e9955e8d18e327
FredrickMbithi <fredrickmbithi000@gmail.com>
2025-11-16 20:25:23 +0300

    fied some errors

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,52 +1,42 @@
-import PostCard from '@/components/common/PostCard';
-import PostModal from '@/components/common/PostModal';
-import Header from '@/components/layout/Header';
-import { PostData, PostProps } from '@/interfaces';
-import { useState } from 'react';
+import React from "react";
+import PostCard from "@/components/common/PostCard";
+import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 
-interface PostsPageProps {
-  posts: PostProps[];
-}
+type PostsPageProps = { posts: PostProps[] };
 
-const Posts: React.FC<PostsPageProps> = ({ posts }) => {
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [localPosts, setLocalPosts] = useState<PostProps[]>(posts);
-
-  const handleAddPost = (newPost: PostData) => {
-    const postWithId: PostProps = { ...newPost, id: localPosts.length + 1 } as PostProps;
-    setLocalPosts(prev => [postWithId, ...prev]);
-  };
-
+const Posts = ({ posts }: PostsPageProps) => {
+  console.log(posts)
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <main className="p-4 flex-grow overflow-y-auto">
+      <main className="p-4">
         <div className="flex justify-between">
-          <h1 className="text-2xl font-semibold">Post Content</h1>
-          <button
-            onClick={() => setModalOpen(true)}
-            className="bg-blue-700 px-4 py-2 rounded-full text-white"
-          >
-            Add Post
-          </button>
+        <h1 className=" text-2xl font-semibold">Post Content</h1>
+        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {localPosts.map(({ title, body, userId, id }) => (
-            <PostCard key={id} title={title} body={body} userId={userId} id={id} />
-          ))}
+        <div className="grid grid-cols-3 gap-2 ">
+          {
+            posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+              <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+            ))
+          }
         </div>
       </main>
-      {isModalOpen && (
-        <PostModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
-      )}
     </div>
-  );
-};
+  )
+}
+
 
 export async function getStaticProps() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const posts = await response.json();
-  return { props: { posts } };
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
 
 export default Posts;
